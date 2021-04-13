688cf0ef81438c3abd3c71d582029e8e8131f2cb
burgundypie <kotyonok2001@mai.ru>
2021-04-13 17:52:56 +0300

    Симонова Арина

--- include/TimedDoor.h
+++ include/TimedDoor.h
@@ -1,52 +1,71 @@
 // Copyright 2021 GHA Test Team
+// Copyright 2021 burgundypie
 
 #ifndef INCLUDE_TIMEDDOOR_H_
 #define INCLUDE_TIMEDDOOR_H_
-
 class DoorTimerAdapter;
 class Timer;
 class Door;
 class TimedDoor;
 
 class TimerClient {
- public:
-  virtual void Timeout() = 0;
+public:
+	virtual void Timeout() = 0;
+	virtual void Timeout() = 0;
 };
 
 class Door {
- public:
-  virtual void lock() = 0;
-  virtual void unlock() = 0;
-  virtual bool isDoorOpened() = 0;
+public:
+	virtual void lock() = 0;
+	virtual void unlock() = 0;
+	virtual bool isDoorOpened() = 0;
+	virtual void lock() = 0;
+	virtual void unlock() = 0;
+	virtual bool isDoorOpened() = 0;
 };
 
 class DoorTimerAdapter : public TimerClient {
- private:
-  TimedDoor& door;
- public:
-  explicit DoorTimerAdapter(TimedDoor&);
-  void Timeout();
+private:
+	TimedDoor& door;
+	TimedDoor& door;
+public:
+	explicit DoorTimerAdapter(TimedDoor&);
+	void Timeout();
+	explicit DoorTimerAdapter(TimedDoor&);
+	void Timeout() override;
 };
 
 class TimedDoor : public Door {
- private:
-  DoorTimerAdapter * adapter;
-  int iTimeout;
-  bool opened;
- public:
-  explicit TimedDoor(int);
-  bool isDoorOpened();
-  void unlock();
-  void lock();
-  void DoorTimeOut();
-  void throwState();
+private:
+	DoorTimerAdapter* adapter;
+	int iTimeout;
+	bool opened;
+	DoorTimerAdapter* adapter;
+	int iTimeout;
+	bool opened;
+public:
+	explicit TimedDoor(int);
+	bool isDoorOpened();
+	void unlock();
+	void lock();
+	void DoorTimeOut();
+	void throwState();
+	explicit TimedDoor(int);
+	bool isDoorOpened() override;
+	void unlock() override;
+	void lock() override;
+	void DoorTimeOut();
+	void throwState();
 };
 
 class Timer {
-  TimerClient *client;
-  void sleep(int);
- public:
-  void tregister(int, TimerClient*);
+	TimerClient* client;
+	void sleep(int);
+	TimerClient* client;
+	void sleep(int);
+public:
+	void tregister(int, TimerClient*);
+	void tregister(int, TimerClient*);
 };
 
 #endif  // INCLUDE_TIMEDDOOR_H_
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 // Copyright 2021 GHA Test Team
+// Copyright 2021 burgundypie
 #include "TimedDoor.h"
 #include <iostream>
 
 int main() {
-  TimedDoor tDoor(5);
-  tDoor.lock();
-  tDoor.unlock();
+	TimedDoor tDoor(5);
+	tDoor.lock();
+	tDoor.unlock();
 
-  return 0;
+	return 0;
+	TimedDoor tDoor(5);
+	tDoor.lock();
+	tDoor.unlock();
+	return 0;
 }
